f7aad325454d0a0c2a0ee82cafdf17dcb6ac3abf
inkyu lee <gofiri@gmail.com>
2020-08-23 16:18:49 +0900

    WIP

--- app/src/controllers/dailies/mod.rs
+++ app/src/controllers/dailies/mod.rs
@@ -6,12 +6,13 @@ pub(super) struct Dailies {}
 impl Dailies {
     pub(super) async fn serve_inner(
         context: Context,
-        _payload: &[u8],
+        payload: &[u8],
         idx: usize,
     ) -> http::Result<http::Response> {
         if let Some(path) = context.request.uri().nth_path(idx) {
             match path.as_ref() {
                 "new" => Ok(Self::new(context)),
+                "create" => Ok(Self::create(context, payload)),
                 article_id => Ok(Self::show(context, article_id)),
             }
         } else {
@@ -26,8 +27,33 @@ impl Dailies {
             &super::render_with_layout(
                 &context,
                 &tent::html!(
-                    article
-                        span.label {format!("New")}
+                    article.signin
+                        header
+                            h1
+                                "Daily"
+                        form action="/dailies/create" method="POST"
+                            input type="date" name="date" placeholder="2020-08-23"
+                            input.submit type="submit" value="SUBMIT"
+                )
+                .to_string(),
+            ),
+        )
+    }
+
+    fn create(context: Context, payload: &[u8]) -> http::Response {
+        http::Response::html(
+            200,
+            vec![],
+            &super::render_with_layout(
+                &context,
+                &tent::html!(
+                    article.signin
+                        header
+                            h1
+                                "Daily"
+                        form action="/dailies/create" method="POST"
+                            input type="date" name="date" placeholder="2020-08-23"
+                            input.submit type="submit" value="SUBMIT"
                 )
                 .to_string(),
             ),
